75337972e753835bcf89f1d6b5050a031b2efe5b
Esteban Mercado <esteban.mercadom99@gmail.com>
2025-12-15 14:24:21 -0600

    Se agrega feature donde el usuario elige el principio y el fin de la lista de FizzBuzzWoof

--- task5.js
+++ task5.js
@@ -1,5 +1,23 @@
 // Refer to Task 5 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Five!");
-  };
+const prompt = require("prompt-sync")();
+const name = prompt("Ingresa tu nombre ");
+const firstNumber = +prompt("Ingresa el primer número donde quieres que empiece el FizzBuzzWoof ")
+const lastNumber = +prompt("Ingresa el último número al que quieres que llegue el FizzBuzzWoof ")
+
+for (i = firstNumber; i < lastNumber + 1; i++){
+  let salida = ""
+
+  if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
+    if(i % 3 == 0)
+      salida += "Fizz"
+    if(i % 5 == 0)
+      salida += "Buzz"
+    if(i % 7 == 0)
+      salida += "Woof"
+  }
+  else
+    salida = i
+    
+  console.log(salida);
+}
