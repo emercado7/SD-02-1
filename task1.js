f7648209328ac535b4c524a08c32163a3a3f3974
Esteban Mercado <esteban.mercadom99@gmail.com>
2025-12-15 14:15:18 -0600

    Se agregó el código del contador de 1 a 105

--- task1.js
+++ task1.js
@@ -1,3 +1,6 @@
 // Refer to Task 1 in your Instructions to complete this task
 const prompt = require("prompt-sync")();
-const name = prompt("Ingresa tu nombre");
+const name = prompt("Ingresa tu nombre ");
+for (let i = 1; i < 106; i++){
+    console.log(i);
+}
