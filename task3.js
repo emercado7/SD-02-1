7d0a81dde24f062f1cee4ba7d4024249caba84f9
Esteban Mercado <esteban.mercadom99@gmail.com>
2025-12-15 14:16:36 -0600

    Se agrega buzz y fizzbuzz con trabajo en conjunto al fizz

--- task3.js
+++ task3.js
@@ -1,5 +1,14 @@
 // Refer to Task 3 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Three!");
-  };
+const prompt = require("prompt-sync")();
+const name = prompt("Ingresa tu nombre");
+for (let i = 1; i < 106; i++){
+  if(i % 3 == 0 && i % 5 == 0)
+    console.log("FizzBuzz")
+  else if (i % 3 == 0)
+    console.log("Fizz")
+  else if (i % 5 == 0)
+    console.log("Buzz")
+  else
+    console.log(i);
+}
